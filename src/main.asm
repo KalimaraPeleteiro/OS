8e4a51b3715ea1bb7aa9cc48ae2a80a898f07e51
Kalimara_Peleteiro <kalimarapeleteiro@gmail.com>
2024-02-20 10:42:58 -0300

    Adicionando mensagem de entrada

--- src/main.asm
+++ src/main.asm
@@ -3,13 +3,54 @@ org 0x7c00
 
 bits 16 ; Para retrocompatibilidade
 
+%define ENDL 0x0D, 0x0A
+
+start:
+    jmp main
+
+
+; Printa algo na tela.
+print:
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb
+    or al, al
+    jz .done
+
+    mov ah, 0x0e
+    mov bh, 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop ax
+    pop bx
+    pop si
+    ret
 
 main:
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    mov ss, ax
+    mov sp, 0x7c00
+
+    mov si, msg
+    call print
+
     hlt ; Interompe a CPU.
 
 
 .halt:
     jmp .halt
+
+
+msg: db "Inicializando Sistema Operacional...", ENDL, 0
 
 ; ===== Criando um Setor de Inicialização de Boot =====
 ; Um setor válido possui 512 bytes.
